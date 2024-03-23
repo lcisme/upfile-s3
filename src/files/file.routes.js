6149f67e77bb98987d0831c5c0516ec8a5d36f65
icisme <lephucuong2003@gmail.com>
2024-03-23 11:58:38 +0700

    heheh

--- src/files/file.routes.js
+++ src/files/file.routes.js
@@ -8,5 +8,6 @@ router.delete("/delete/:s3Key", fileController.deleteFile);
 router.patch("/update/:s3Key", fileController.updateFile);
 router.post("/move/:s3Key", fileController.moveFile);
 router.post("/copy/:s3Key", fileController.copyFile);
+router.post("/copy1/:s3Key1", fileController.copyFile);
 
 module.exports = router;
